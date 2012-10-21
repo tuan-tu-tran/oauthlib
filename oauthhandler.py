c25fe1fa05e98cb7aaca51692f5fa07416a57270
Tuan-Tu TRAN <tuantu.t@gmail.com>
2012-10-21 09:59:44 +0200

    adapted to OAuth case

--- oauthhandler.py
+++ oauthhandler.py
@@ -1,12 +1,9 @@
-class HTTPDigestAuthHandler(BaseHandler, AbstractDigestAuthHandler):
-    """An authentication protocol defined by RFC 2069
+import urllib2
 
-    Digest authentication improves on basic authentication because it
-    does not transmit passwords in the clear.
-    """
+class OAuthHandler(urllib2.BaseHandler, AbstractOAuthHandler):
+    """An authentication protocol defined by RFC 5849"""
 
     auth_header = 'Authorization'
-    handler_order = 490  # before Basic auth
 
     def http_error_401(self, req, fp, code, msg, headers):
         host = urlparse.urlparse(req.get_full_url())[1]
